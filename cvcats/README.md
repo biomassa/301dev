c36a9a37e64ad5df20cb556465541b24a2ea72d8
biomassa <kustota@gmail.com>
2021-10-17 01:18:40 +0300

    Initialize

---
+++ cvcats/README.md
@@ -0,0 +1,4 @@
+CVCATS
+======
+
+Just odevices' "step3" euclydian sequencer tutorial with CV control over the number of triggers added.
---
+++ msproc/toc.lua
@@ -0,0 +1,14 @@
+return {
+  title = "msproc",
+  author = "ars",
+  name = "msproc",
+  units = {
+    {
+      -- Title and category control how this unit appears in the unit browser.
+      title = "msproc",
+      category = "Learning",
+      -- Which lua module contains the unit definition?
+      moduleName = "msproc",
+    },
+  }
+}
